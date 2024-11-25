d2bb2d284ff839ceb39c82def47561d552ce1775
thejas01 <thejasgs19@gmail.com>
2024-11-25 13:59:16 +0530

    Initial Updated files

---
+++ product-service/src/main/java/com/example/product_service/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.example.product_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public String getProducts() {
+        return "Product Service: List of Users";
+    }
+    
+}
